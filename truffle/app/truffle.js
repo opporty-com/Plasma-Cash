ef7ad52895d6125d5c467a66d42400fac0d92102
Vladimir Kovalchuk <vladimir.k@singree.com>
2018-05-07 23:29:46 +0300

    rinkeby config

--- truffle/app/truffle.js
+++ truffle/app/truffle.js
@@ -9,6 +9,12 @@ module.exports = {
       host: "127.0.0.1",
       port: 8546,
       network_id: "*" // Match any network id
+    },
+    ropsten: {
+      host: "127.0.0.1",
+      port: 8545,
+      gas:   4700000,
+      network_id: 3 // Match any network id
     }
   }
 };
