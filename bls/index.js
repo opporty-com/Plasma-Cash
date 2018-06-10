7ed059d2bd93762b3c7c0d64b4b37670fe6ad649
Vladimir Kovalchuk <vladimir.k@singree.com>
2018-06-10 09:56:41 +0300

    ate

--- bls/index.js
+++ bls/index.js
@@ -37,28 +37,26 @@ console.log('  verify(H, sQ, sH) -> e(sQ, H(m)) = e(Q, s H(m)<br><br>')
 console.log('-+*+-+*+-+*+-+*+-+*+-+*+-+*+-+*+-+*+-+*+-+*+-+*+-+*+-+*+-+*+-')
 
 for (let b of br) {
-    console.time('t'+b)
     const bn = new Parameters(b);
     const E  = new Curve(bn);
     const Et = new Curve2(E);
     const pair = new Pairing(Et);
-    
+    console.time('sign');
+
     const s = bigInt(5);
     let H = Et.pointFactory(rng);
     const Q = E.pointFactory(rng);
     const sQ = Q.multiply(s);
     const sH = BLSSigner.sign(Et,  H,  s);
     const sH2 = BLSSigner.sign(Et, H, 4);
-    
     console.log('\x1b[37m', ' Q ('+b+') = (' + (Q.x)+ ', ' +(Q.y)+ ', ' +(Q.z)+')');
     console.log('\x1b[37m', ' sQ('+b+') = (' + (sQ.x)+ ', ' +(sQ.y)+ ', ' +(sQ.z)+')');
     console.log('\x1b[37m', ' H ('+b+') = (' + (H.x.re)+ ', ' +(H.y.re)+ ', ' +(H.z.re)+')');
     console.log('\x1b[37m', ' sH('+b+') = (' + (sH.x.re)+ ', ' +(sH.y.re)+ ', ' +(sH.z.re)+')');
-
     console.log( '\x1b[33m', ' Verify(Q,sQ,H,sH) = ' + BLSSigner.verify(pair, Q,H,sQ,sH)+'');
-    console.timeEnd('t'+b)
-
     console.log('\x1b[31m', ' Verify(Q,sQ,H,s2H) = ' + BLSSigner.verify(pair, Q,H,sQ,sH2)+'');
-    
+
+    console.timeEnd('sign');
+ 
     console.log('\x1b[32m','-+*+-+*+-+*+-+*+-+*+-+*+-+*+-+*+-+*+-+*+-+*+-+*+-+*+-+*+-+*+-')
 }
