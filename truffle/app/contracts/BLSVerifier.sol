b3c462378c3a3ab1d19628324ec6fdce75ef4ce4
Vladimir Kovalchuk <vladimir.k@singree.com>
2018-11-26 20:20:05 +0200

    BLS Verifier contract

---
+++ truffle/app/contracts/BLSVerifier.sol
@@ -0,0 +1,128 @@
+
+pragma solidity ^0.4.21;
+
+library Pairing {
+    struct G1Point {
+        uint X;
+        uint Y;
+    }
+    // Encoding of field elements is: X[0] * z + X[1]
+    struct G2Point {
+        uint[2] X;
+        uint[2] Y;
+    }
+    /// @return the generator of G1
+    function P1() pure internal returns (G1Point) {
+        return G1Point(1, 2);
+    }
+    /// @return the generator of G2
+    function P2() pure internal returns (G2Point) {
+        return G2Point(
+            [11559732032986387107991004021392285783925812861821192530917403151452391805634,
+             10857046999023057135944570762232829481370756359578518086990519993285655852781],
+            [4082367875863433681332203403145435568316851327593401208105741076214120093531,
+             8495653923123431417604973247489272438418190587263600148770280649306958101930]
+        );
+    }
+    /// @return the negation of p, i.e. p.addition(p.negate()) should be zero.
+    function negate(G1Point memory p) internal pure returns (G1Point)  {
+        // The prime q in the base field F_q for G1
+        uint q = 21888242871839275222246405745257275088696311157297823662689037894645226208583;
+        if (p.X == 0 && p.Y == 0)
+            return G1Point(0, 0);
+        return G1Point(p.X, q - (p.Y % q));
+    }
+    
+    /// @return the result of computing the pairing check
+    /// e(p1[0], p2[0]) *  .... * e(p1[n], p2[n]) == 1
+    /// For example pairing([P1(), P1().negate()], [P2(), P2()]) should
+    /// return true.
+    function pairing(G1Point[] memory p1, G2Point[] memory p2) internal returns (bool) {
+        require(p1.length == p2.length);
+        uint elements = p1.length;
+        uint inputSize = elements * 6;
+        uint[] memory input = new uint[](inputSize);
+        for (uint i = 0; i < elements; i++)
+        {
+            input[i * 6 + 0] = p1[i].X;
+            input[i * 6 + 1] = p1[i].Y;
+            input[i * 6 + 2] = p2[i].X[0];
+            input[i * 6 + 3] = p2[i].X[1];
+            input[i * 6 + 4] = p2[i].Y[0];
+            input[i * 6 + 5] = p2[i].Y[1];
+        }
+        uint[1] memory out;
+        bool success;
+        assembly {
+            success := call(sub(gas, 2000), 8, 0, add(input, 0x20), mul(inputSize, 0x20), out, 0x20)
+            // Use "invalid" to make gas estimation work
+            switch success case 0 { invalid() }
+        }
+        require(success);
+        return out[0] != 0;
+    }
+    /// Convenience method for a pairing check for two pairs.
+    function pairingProd2(G1Point memory a1, G2Point memory a2, G1Point memory b1, G2Point memory b2) internal returns (bool) {
+        G1Point[] memory p1 = new G1Point[](2);
+        G2Point[] memory p2 = new G2Point[](2);
+        p1[0] = a1;
+        p1[1] = b1;
+        p2[0] = a2;
+        p2[1] = b2;
+        return pairing(p1, p2);
+    }
+    /// Convenience method for a pairing check for three pairs.
+    function pairingProd3(G1Point memory a1, G2Point memory a2,
+        G1Point memory b1, G2Point memory b2,
+        G1Point memory c1, G2Point memory c2
+    ) internal returns (bool) {
+        G1Point[] memory p1 = new G1Point[](3);
+        G2Point[] memory p2 = new G2Point[](3);
+        p1[0] = a1;
+        p1[1] = b1;
+        p1[2] = c1;
+        p2[0] = a2;
+        p2[1] = b2;
+        p2[2] = c2;
+        return pairing(p1, p2);
+    }
+    /// Convenience method for a pairing check for four pairs.
+    function pairingProd4(
+        G1Point memory a1, G2Point memory a2,
+        G1Point memory b1, G2Point memory b2,
+        G1Point memory c1, G2Point memory c2,
+        G1Point memory d1, G2Point memory d2
+    ) internal returns (bool) {
+        G1Point[] memory p1 = new G1Point[](4);
+        G2Point[] memory p2 = new G2Point[](4);
+        p1[0] = a1;
+        p1[1] = b1;
+        p1[2] = c1;
+        p1[3] = d1;
+        p2[0] = a2;
+        p2[1] = b2;
+        p2[2] = c2;
+        p2[3] = d2;
+        return pairing(p1, p2);
+    }
+}
+contract BLSVerifier {
+    using Pairing for *;
+    
+    event Pair(bool);
+
+    function verifyingKey(
+        uint Qx, uint Qy, uint publicKeyx, uint publiKeyy,
+        uint[] hashx, uint[] hashy, uint[] signx, uint[] signy) public returns (bool) {
+        Pairing.G1Point memory Q = Pairing.G1Point (1914471741068526290895274035825834163057076654219923905096232007248416825259, 2441287986430703333778158542217266776955904719383828356926512700796342019369);
+        Pairing.G1Point memory publicKey = Pairing.G1Point(13060711567309942139127343122189677326498408300853229865518797107801319824296, 2464980274216463764855581962546674934025981134706187530645499576303044495323);
+        Pairing.G2Point memory hash = Pairing.G2Point([14606578554303469723495585232984845656264509420570646315192863970415210201557, 8725532381388665809230708976569529227500296992636754297134929236741816665538],[2346678989646855296475216004611579000476122428477843049131403719605101387582, 1486158328251987417116360447730741840937312108834614544561773925813873744345]);
+        Pairing.G2Point memory sign = Pairing.G2Point([2474968942602878055147817517715317772699895426582175478936878850461321093278, 12812377419359504388560611417326677428520244018810228598905013392490169035179],[600386518136062853270390266572190894692638608046050040681033378627649963550, 7225887162421946457503648363984771747879028053171533685103972394543293219530]);
+
+        bool f = Pairing.pairingProd2(publicKey,hash,Pairing.negate(Q),sign);
+        
+        emit Pair(f);
+        
+        return f;
+    }
+}
