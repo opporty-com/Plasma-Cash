b964ea1ca4bac8829997570a34a0c19e6939400a
Oleksandr Nashyvan <alex@nashivan.pp.ua>
2019-09-02 15:05:45 +0300

    refactoring

--- plasma-core/src/root-chain/web3.js
+++ plasma-core/src/root-chain/web3.js
@@ -5,19 +5,20 @@ import Web3 from 'web3'
 import config from '../config'
 import fs from 'fs'
 
-let provider;
+let web3;
 
 if (config.web3WsProvider) {
   console.log(`Set WS WEB3 provider ${config.web3WsProvider}`);
-  provider = new Web3.providers.WebsocketProvider(config.web3WsProvider)
+  // provider = new Web3.providers.WebsocketProvider(config.web3WsProvider)
+  web3 = new Web3(config.web3WsProvider)
 } else {
   if (!fs.existsSync(config.gethIpc)) {
     throw new Error(`Not exists geth.ipc  ${config.gethIpc}`)
   }
   console.log(`Set WS WEB3 provider ${config.web3WsProvider}`);
-  provider = new Web3.providers.IpcProvider(config.gethIpc, net)
+  const provider = new Web3.providers.IpcProvider(config.gethIpc, net)
+  web3 = new Web3(provider);
 }
 
-let web3 = new Web3(provider)
 
 export default web3
