454733308743a9e938969f9e5e7d7311102cb233
Oleksandr Nashyvan <alex@nashivan.pp.ua>
2019-09-07 18:02:15 +0300

    add timestamp

--- plasma-core/src/child-chain/controllers/Block.js
+++ plasma-core/src/child-chain/controllers/Block.js
@@ -58,8 +58,8 @@ async function submitted({operator, merkleRoot, blockNumber}) {
 async function get(number) {
   const block = await BlockModel.get(number);
   if (!block) throw new Error("Block not found!");
-  logger.debug("get block");
-  logger.debug(block.getJson());
+  logger.debug("get block", block);
+  logger.debug(JSON.stringify(block.getJson()));
   return block.getJson();
 }
 
