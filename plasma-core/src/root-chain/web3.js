a98e0894cc38ce9f05915af4f244e65e383390de
Oleksandr Nashyvan <alex@nashivan.pp.ua>
2019-09-27 14:36:41 +0300

    fix web3

--- plasma-core/src/root-chain/web3.js
+++ plasma-core/src/root-chain/web3.js
@@ -10,15 +10,21 @@ let web3;
 if (config.web3WsProvider) {
   console.log(`Set WS WEB3 provider ${config.web3WsProvider}`);
   const getProvider = () => {
-    const provider = new Web3.providers.WebsocketProvider(config.web3WsProvider)
-    provider.on('connect', () => console.log('WS Connected'))
+    const provider = new Web3.providers.WebsocketProvider(config.web3WsProvider);
+    provider.on('connect', () => {
+      console.log('WS Connected')
+      provider.emit("restartWatchEvents");
+    })
     provider.on('error', e => {
       console.error('WS Error', e)
+      provider.emit("restartWatchEvents");
       web3.setProvider(getProvider())
     });
     provider.on('end', e => {
-      console.error('WS End', e)
+      console.error('WS End', e);
+      provider.emit("restartWatchEvents");
       web3.setProvider(getProvider())
+
     });
 
     return provider
